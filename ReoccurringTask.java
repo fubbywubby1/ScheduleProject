e91e2de2e05f95b4bdedf61b8830953498dc580c
mj-obrien <mollyjeobrien@gmail.com>
2025-04-15 11:11:25 -0400

    Update ReoccurringTask.java

--- ReoccurringTask.java
+++ ReoccurringTask.java
@@ -1,3 +1,16 @@
 public class ReoccurringTask extends Task implements Reoccuring {
-    
+   private FrequencyType frequencyType;
+
+    // other stuff tba
+
+    // getter for frequency type
+    @Override
+    public FrequencyType getFrequencyType() {
+       return this.frequencyType;
+    }
+
+    // setter for frequency type
+    public void setFrequencyType(FrequencyType frequencyType) {
+        this.frequencyType = frequencyType;
+    } 
 }
