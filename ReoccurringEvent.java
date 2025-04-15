c4db49760fcf20ee4d464ab9cbe64a2ffaa927f8
mj-obrien <mollyjeobrien@gmail.com>
2025-04-15 11:10:57 -0400

    Update ReoccurringEvent.java

--- ReoccurringEvent.java
+++ ReoccurringEvent.java
@@ -1,3 +1,16 @@
 public class ReoccurringEvent extends Event implements Reoccurring {
-    
+    private FrequencyType frequencyType;
+
+    // other stuff tba
+
+    // getter for frequency type
+    @Override
+    public FrequencyType getFrequencyType() {
+       return this.frequencyType;
+    }
+
+    // setter for frequency type
+    public void setFrequencyType(FrequencyType frequencyType) {
+        this.frequencyType = frequencyType;
+    }
 }
