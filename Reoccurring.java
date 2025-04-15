88492cded8f04e9d166dcef3442cb630d465fb3a
mj-obrien <mollyjeobrien@gmail.com>
2025-04-15 11:21:59 -0400

    enums :P

--- Reoccurring.java
+++ Reoccurring.java
@@ -2,6 +2,7 @@ interface Reoccurring {
     FrequencyType getFrequencyType(); //DAILY, WEEKLY, MONTHLY, YEARLY
 }
 
+// enums to define frequency of reoccurrance 
 public enum FrequencyType {
     DAILY,
     WEEKLY,
