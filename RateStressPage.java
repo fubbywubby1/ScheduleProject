dd7358020bb3052b4547eff28e449077aca3e57c
schwar74 <schwar74@students.rowan.edu>
2025-04-25 00:18:09 -0400

    small change

--- RateStressPage.java
+++ RateStressPage.java
@@ -2,8 +2,11 @@ import java.util.HashMap;
 
 public class RateStressPage extends Application{
     // one page, and a Slider for each Label.
-    static HashMap<Label, Integer> stressLevels = new HashMap<>();
+    static ArrayList<Integer> stressLevels = new ArrayList<>();
 
+    // sliders for each Label's stress levels
+    
+    
 
     // pass this information to the Self Care scheduler.
 
