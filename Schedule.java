b5fcbce8e07625446c13b6472730b4bba163ca11
fubbywubby <alexanderts06@gmail.com>
2025-04-29 10:28:32 -0400

    schedule

--- Schedule.java
+++ Schedule.java
@@ -1,7 +1,18 @@
 import java.util.HashMap;
 
 public class Schedule {
-    private HashMap<DaysOfTheWeek, HashMap<TimeChunk, Event>> scheduleMap;
+    public HashMap<DaysOfTheWeek, HashMap<TimeChunk, Event>> scheduleMap;
 
+    public Schedule(HashMap<DaysOfTheWeek, HashMap<TimeChunk, Event>> scheduleMap) {
+        this.scheduleMap = scheduleMap;
+    }
+
+    public HashMap<TimeChunk, Event> getDaySchedule(DaysOfTheWeek day) throws Exception {
+        if (day != null) {
+            return scheduleMap.get(day);
+        } else {
+            throw new Exception("Invalid value for inputted day");
+        }
+    }
     
 }
