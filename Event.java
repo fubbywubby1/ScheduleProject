5316875430e7f0a12649e6844b79dfe656d4a72b
fubbywubby <alexanderts06@gmail.com>
2025-04-30 16:46:39 -0400

    jabbadoc2

--- Event.java
+++ Event.java
@@ -1,44 +1,40 @@
+/**
+ * An extension of TimeBlockable, except is has a label
+ * Though this doesn't have direct functionality, it is extremely 
+ * relevant to the functionality of SelfCareScheduler
+ * 
+ * @author Alexander Simonson, Emily Schwartz, Douglas Tranz and Molly O'Brien
+ */
 class Event extends TimeBlockable {
     private Label label;
     private Boolean isTask;
 
+    /**
+     * Initializes all instance vars
+     * @param name is set to name
+     * @param description is set to description
+     * @param label is set to label
+     * @param isTask is set to istask
+     */
     public Event(String name, String description, Label label, Boolean isTask) {
         super(name, description);
         this.label = label;
         this.isTask = isTask;
     }
 
+    /**
+     * returns label
+     * @return label
+     */
     public Label getLabel() {
         return label;
     }
 
+    /**
+     * returns istask
+     * @return istask
+     */
     public Boolean getIsTask() {
         return isTask;
     }
-
-    /* 
-    public void setStartTime(int startTime) throws Exception {
-        if (startTime >= 0 && startTime < 2400) {
-            if (TimeHandler.checkTimeConflict(new Event(this.name, this.description, this.priority, startTime, this.endTime)) == false) {
-                throw new Exception("Time Conflict");
-            } else {
-                this.startTime = startTime;
-            }
-        } else {
-            throw new Exception("Invalid Start time");
-        }
-    }
-
-    public void setEndTime(int endTime) throws Exception{
-        if (endTime >= 0 && endTime < 2400) {
-            if (TimeHandler.checkTimeConflict(new Event(this.name, this.description, this.priority, this.startTime, endTime)) == false) {
-                throw new Exception("Time Conflict");
-            } else {
-                this.endTime = endTime;
-            }
-        } else {
-            throw new Exception("Invalid end time");
-        }
-    }
-    */
 }
