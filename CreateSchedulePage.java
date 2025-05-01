49e4bb65089e964775435e0af2227728de5315da
schwar74 <schwar74@students.rowan.edu>
2025-04-30 21:38:13 -0400

    fixed

--- CreateSchedulePage.java
+++ CreateSchedulePage.java
@@ -4,135 +4,138 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 import java.time.LocalTime;
-import java.util.HashMap;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * This is the CreateSchedulePage class.
  * It generates the GUI for the user to input their own schedule, and it passes
  * that information to the Schedule class, which populates their schedule.
- * When its done, it will pass the user to the RateStressPage.
+ * When it's done, it will pass the user to the RateStressPage.
  * 
  * @author Alexander Simonson, Emily Schwartz, Douglas Tranz and Molly O'Brien
  */
 public class CreateSchedulePage extends Application {
-    @Override
-    public void start(Stage primaryStage)
-    {
-        String scheduleName;
-        String eventName;
-        String eventDescription;
-        String day;
-        String startTime;
-        String endTime;
-        ArrayList<String> labelStrings = new ArrayList<>();
-        int numLabels = Label.values().length;
+    private String scheduleName = null; 
+    private boolean firstRun;
 
-        for (Label l : Label.values())
-        {
-            labelStrings.add(l.getName());
-        }
-        String label;
-        Event event;
-        boolean firstRun = true;
+
+    @Override
+    public void start(Stage primaryStage) {
+        firstRun = true;
 
         primaryStage.setTitle("Create a New Schedule");
+
         TextField nameTextBox = new TextField("Enter Schedule Name");
         TextField eventTextBox = new TextField("Enter Event Name");
         TextField eventDescriptionTextBox = new TextField("Enter Event Description");
-        
+
         ComboBox<String> dayComboBox = new ComboBox<>();
-        // for each DaysOfTheWeek enum, cast as a string and put it into an array
         for (DaysOfTheWeek d : DaysOfTheWeek.values()) {
-            dayComboBox.getItems().add(day.toString());
+            dayComboBox.getItems().add(d.name());
         }
         dayComboBox.setPromptText("Choose Day");
 
-        TextField hourTextBoxA = new TextField(" (24 hour format) Enter Start Hour");
-        TextField minuteTextBoxA = new TextField("Enter Start Minute");
-        TextField hourTextBoxB = new TextField("Enter End Hour");
-        TextField minuteTextBoxB = new TextField("Enter End Minute");
+        TextField hourTextBoxA = new TextField("Start Hour (24hr)");
+        TextField minuteTextBoxA = new TextField("Start Minute");
+        TextField hourTextBoxB = new TextField("End Hour (24hr)");
+        TextField minuteTextBoxB = new TextField("End Minute");
+
+        ArrayList<String> labelStrings = new ArrayList<>();
+        for (Label l : Label.values()) {
+            labelStrings.add(l.getName());
+        }
 
         ComboBox<String> labelComboBox = new ComboBox<>();
-        labelComboBox.getItems().addAll(
-            labelStrings.toArray(new String[numLabels])
-        );
+        labelComboBox.getItems().addAll(labelStrings);
         labelComboBox.setPromptText("Choose Label");
 
-        // Text area to output error messages
         TextArea errorTextArea = new TextArea();
+        errorTextArea.setEditable(false);
+        errorTextArea.setWrapText(true);
 
         Button addEventButton = new Button("Add Event");
         addEventButton.setStyle("-fx-font-size: 16px; -fx-padding: 10px;");
         addEventButton.setOnAction(e -> {
             if (firstRun) {
-                // Check if the schedule name is empty
                 if (nameTextBox.getText().isEmpty()) {
                     scheduleName = "Default Schedule";
-                    return;
+                } else {
+                    scheduleName = nameTextBox.getText();
                 }
-                scheduleName = nameTextBox.getText();
-                // create a new schedule object with this name
                 Schedule.setName(scheduleName);
                 firstRun = false;
                 nameTextBox.setDisable(true);
             }
-            
 
-            eventName = eventTextBox.getText();
-            day = dayComboBox.getValue();
-            startTime = hourTextBoxA.getText() + ":" + minuteTextBoxA.getText();
-            endTime = hourTextBoxB.getText() + ":" + minuteTextBoxB.getText();
-            label = labelComboBox.getValue();
+            String eventName = eventTextBox.getText();
+            String eventDescription = eventDescriptionTextBox.getText();
+            String day = dayComboBox.getValue();
+            String labelName = labelComboBox.getValue();
 
-            // relabel the label
-            Label enumLabel;
-            for (String l : labelStrings)
-            {
-                for (Label labelEnum : Label.values())
-                {
-                    if (labelEnum.getName().equals(l))
-                    {
-                        enumLabel = labelEnum;
-                        break;
-                    }
+            if (day == null || labelName == null || eventName.isEmpty()) {
+                errorTextArea.appendText("Please fill all required fields.\n");
+                return;
+            }
+
+            DaysOfTheWeek enumDay;
+            try {
+                enumDay = DaysOfTheWeek.valueOf(day);
+            } catch (IllegalArgumentException ex) {
+                errorTextArea.appendText("Invalid day selected.\n");
+                return;
+            }
+
+            Label enumLabel = null;
+            for (Label l : Label.values()) {
+                if (l.getName().equals(labelName)) {
+                    enumLabel = l;
+                    break;
                 }
             }
-
-            // Create LocalTimes
-            LocalTime localStartTime = LocalTime.parse(startTime);
-            LocalTime localEndTime = LocalTime.parse(endTime);
-
-            TimeChunk eventChunk = new TimeChunk(localStartTime, localEndTime);
-            // Create a new event object
-            event = new Event(eventName, eventDescription, enumLabel, false);
-
-            // passing it to the hashmap, and if it fails, catch the exception and print it
-            try {
-                TimeHandler.addToTimeBlock(eventChunk, event, Schedule.scheduleMap.get(day));
-            } catch (UnableToScheduleException e) {
-                errorTextArea.appendText(e.toString() + "\n");
+            if (enumLabel == null) {
+                errorTextArea.appendText("Invalid label selected.\n");
+                return;
             }
 
-            // Clear the text fields
-            nameTextBox.clear();
+            try {
+                LocalTime localStartTime = LocalTime.parse(hourTextBoxA.getText() + ":" + minuteTextBoxA.getText());
+                LocalTime localEndTime = LocalTime.parse(hourTextBoxB.getText() + ":" + minuteTextBoxB.getText());
+
+                TimeChunk eventChunk = new TimeChunk(localStartTime, localEndTime);
+                Event event = new Event(eventName, eventDescription, enumLabel, false);
+
+                TimeHandler.addToTimeBlock(eventChunk, event, Schedule.scheduleMap.get(enumDay));
+            } catch (UnableToScheduleException ex) {
+                errorTextArea.appendText("Scheduling error: " + ex.getMessage() + "\n");
+            } catch (Exception ex) {
+                errorTextArea.appendText("Error: " + ex.getMessage() + "\n");
+            }
+
+            // Clear fields for next event (except schedule name)
             eventTextBox.clear();
+            eventDescriptionTextBox.clear();
+            dayComboBox.getSelectionModel().clearSelection();
+            labelComboBox.getSelectionModel().clearSelection();
             hourTextBoxA.clear();
             minuteTextBoxA.clear();
             hourTextBoxB.clear();
             minuteTextBoxB.clear();
-            
         });
 
         Button rateStressButton = new Button("Continue to Rate Stress");
         rateStressButton.setStyle("-fx-font-size: 16px; -fx-padding: 10px;");
-        rateStressButton.setOnAction(a -> {
-            ScheduleGUI.startRateStressPage(primaryStage);
-        });
+        rateStressButton.setOnAction(a -> ScheduleGUI.startRateStressPage(primaryStage));
+
+        VBox layout = new VBox(10, nameTextBox, eventTextBox, eventDescriptionTextBox, dayComboBox,
+                hourTextBoxA, minuteTextBoxA, hourTextBoxB, minuteTextBoxB,
+                labelComboBox, addEventButton, rateStressButton, errorTextArea);
+        layout.setStyle("-fx-padding: 20;");
+        Scene scene = new Scene(layout, 450, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
