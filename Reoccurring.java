4b666a1adf54da702c313ecd8197ee4d36cba8ca
mj-obrien <mollyjeobrien@gmail.com>
2025-05-01 10:27:46 -0400

    Update Reoccurring.java

--- Reoccurring.java
+++ Reoccurring.java
@@ -1,10 +1,11 @@
 import java.util.Set;
 
-/*
-*This interface allows implementing classes to specify which days they recur on,
-*and provides methods to check, add, or remove recurrence days.
-*It is designed to work within a one-week scheduling system.
+/**
+* This interface allows implementing classes to specify which days they recur on,
+* and provides methods to check, add, or remove recurrence days.
+* It is designed to work within a one-week scheduling system.
 *
+* @author Alexander Simonson, Emily Schwartz, Douglas Tranz and Molly O'Brien
 */
 interface Reoccurring {
 
