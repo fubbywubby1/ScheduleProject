8794d1f461b775ee13ea80c92bc855b10c98b059
mj-obrien <mollyjeobrien@gmail.com>
2025-05-01 10:33:17 -0400

    doccing it

--- Reoccurring.java
+++ Reoccurring.java
@@ -9,10 +9,35 @@ import java.util.Set;
 */
 interface Reoccurring {
 
+    /**
+    * returns the set of days on which an item repeats
+    * @return the recurring days as a set
+    */
     Set<DaysOfTheWeek> getRecurringDays();
+
+    /**
+    * replaces the current recurring days with a new set
+    * @param days is the new set of recurring days
+    */
     void setRecurringDays(Set<DaysOfTheWeek> days); 
+
+    /**
+    * checks whether or not the item is supposed to happen on the given day
+    * @param day is the day to check
+    * @return true if it occurs on that day, false otherwise
+    */
     boolean occursOn(DaysOfTheWeek day); 
+
+    /**
+    * adds a single day to the list of recurring days
+    * @param day is the day to add
+    */
     void addRecurringDay(DaysOfTheWeek day);  
+
+    /**
+    * removes a single day from the list of recurring days
+    * @param day is the day to remove
+    */
     void removeRecurringDay(DaysOfTheWeek day); 
 
 }
